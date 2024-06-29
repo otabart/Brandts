e87ad6ef7166b46c4fc583cf50409f05491bd818
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-06-29 16:37:25 +0100

    Fix: fix cors settings for delete request

--- Backend/src/middlewares/index.middleware.ts
+++ Backend/src/middlewares/index.middleware.ts
@@ -15,7 +15,7 @@ export default (app: Application) => {
   // CORS middleware
   app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
 
